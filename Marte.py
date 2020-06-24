7f7fb1ebe619a946c6da161545ca31297a9c7894
DenilsonMoralesA <67348944+DenilsonMoralesA@users.noreply.github.com>
2020-06-24 11:32:10 -0500

    Delete Marte.py

--- Marte.py
+++
@@ -1,7 +0,0 @@
-distancia_marte_km= 249100000
-velocidad_luz_kms=300000
-tiempo_segundos=distancia_marte_km/velocidad_luz_kms
-print("La luz a marte desde el sol trada en llegar", tiempo_segundos, "segundos")
-tiempo_minutos=tiempo_segundos/60
-print("o lo equivalente en", tiempo_minutos, "minutos")
-
