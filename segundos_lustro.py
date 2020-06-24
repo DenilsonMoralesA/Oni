2de9cc3373718542ea20640e00dd38ca663cf601
DenilsonMoralesA <67348944+DenilsonMoralesA@users.noreply.github.com>
2020-06-24 11:32:57 -0500

    Delete segundos_lustro.py

--- segundos_lustro.py
+++
@@ -1,8 +0,0 @@
-lustro_en_años=5
-dias_a_horas=24
-horas_a_segundos=3600
-años_a_dias=365
-lustro=lustro_en_años*años_a_dias*dias_a_horas*horas_a_segundos
-print("Sabias que un lustro tiene", lustro, "segundos :o")
-
-
